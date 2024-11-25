f5d5e37bd3ede5da48b0bad263cbec1972955949
Jon Erickson <jon@deschutesdesigngroup.com>
2024-11-25 11:40:23 -0800

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        URL::forceHttps();
+    }
+}
